d0bb0a2de068ba8b49fa5a36702780f8f2d68dc9
jcalendo <jcalendo@protonmail.com>
2023-03-07 16:48:17 -0500

    Remove old reference creation script

--- data-raw/create-reference.R
+++ data-raw/create-reference.R
@@ -1,123 +1,98 @@
-# Create reference by extracting ranges from PharmVar haplotype.tsv files
+# Create Reference Ranges from PharmVar database
 #
-# ------------------------------------------------------------------------------
-suppressPackageStartupMessages(library(data.table))
+# -----------------------------------------------------------------------------
+suppressPackageStartupMessages(library(VariantAnnotation))
 
-# message("Downloading PharmVar Database...")
-# url <- "https://www.pharmvar.org/get-download-file?name=ALL&refSeq=ALL&fileType=zip&version=current"
-# zipped <- tempfile()
-# unzipped <- tempdir()
-# download.file(url, destfile = zipped)
-#
-# message("Extracting PharmVarDB...")
-# fpaths <- utils::unzip(zipped, overwrite = TRUE, exdir = unzipped)
 
+# File operations ---------------------------------------------------------
 fpaths <- list.files(
-    "data-raw/pharmvar-5.2.13",
-    pattern = "*.tsv",
+    path = "data-raw/pharmvar-5.2.13", 
+    pattern = "*.vcf",
     recursive = TRUE,
     full.names = TRUE
 )
 
-grch38_files <- grep("GRCh38", fpaths, value = TRUE)
-grch37_files <- grep("GRCh37", fpaths, value = TRUE)
-names(grch38_files) <- gsub("\\.NC.*", "", basename(grch38_files))
-names(grch37_files) <- gsub("\\.NC.*", "", basename(grch37_files))
+# Select GRCh38 and GRCh37 files separately
+grch37_vcf <- fpaths[grepl("GRCh37", fpaths)]
+grch38_vcf <- fpaths[grepl("GRCh38", fpaths)]
 
-grch38_dt <- rbindlist(lapply(grch38_files, fread))
-grch37_dt <- rbindlist(lapply(grch37_files, fread))
+# Keep only major allele definitions
+grch37_vcf <- grch37_vcf[!grepl("\\.[0-9]+\\.vcf$", grch37_vcf)]
+grch38_vcf <- grch38_vcf[!grepl("\\.[0-9]+\\.vcf$", grch38_vcf)]
 
-# Filter for main alleles
-grch38_dt <- grch38_dt[!`Haplotype Name` %like% "\\.[0-9]+$"]
-grch37_dt <- grch37_dt[!`Haplotype Name` %like% "\\.[0-9]+$"]
-grch38_dt <- grch38_dt[!`Haplotype Name` %like% "^rs[0-9]+"]
-grch37_dt <- grch37_dt[!`Haplotype Name` %like% "^rs[0-9]+"]
-grch38_dt <- grch38_dt[ReferenceSequence != "REFERENCE"]
-grch37_dt <- grch37_dt[ReferenceSequence != "REFERENCE"]
-grch38_dt <- grch38_dt[!`Haplotype Name` %like% "\\*1$"]
-grch37_dt <- grch37_dt[!`Haplotype Name` %like% "\\*1$"]
+# Name by the haplotype
+names(grch37_vcf) <- tools::file_path_sans_ext(basename(grch37_vcf))
+names(grch38_vcf) <- tools::file_path_sans_ext(basename(grch38_vcf))
 
-# Map from RefSeq IDs to UCSC IDs
-hg38_alias <- data.table::data.table(
-    refseq = c("NC_000001.11", "NC_000010.11", "NC_000011.10", "NC_000012.12",
-               "NC_000013.11", "NC_000014.9", "NC_000015.10", "NC_000016.10",
-               "NC_000017.11", "NC_000018.10", "NC_000019.10", "NC_000002.12",
-               "NC_000020.11", "NC_000021.9", "NC_000022.11", "NC_000003.12",
-               "NC_000004.12", "NC_000005.10", "NC_000006.12", "NC_000007.14",
-               "NC_000008.11", "NC_000009.12", "NC_012920.1", "NC_000023.11",
-               "NC_000024.10"),
-    ucsc = c("chr1", "chr10", "chr11", "chr12", "chr13", "chr14", "chr15",
-             "chr16", "chr17", "chr18", "chr19", "chr2", "chr20", "chr21",
-             "chr22", "chr3", "chr4", "chr5", "chr6", "chr7", "chr8", "chr9",
-             "chrM", "chrX", "chrY"),
-    key = "refseq"
+# Swap "_" for "*"
+names(grch37_vcf) <- gsub("_", "*", names(grch37_vcf))
+names(grch38_vcf) <- gsub("_", "*", names(grch38_vcf))
+
+# Read in data as ranges --------------------------------------------------
+
+# Read in separate lists for each genome build
+grch37_haplotype_ranges <- parallel::mclapply(
+    grch37_vcf,
+    readVcfAsVRanges,
+    genome = "hg19",
+    mc.cores = 4
+)
+grch38_haplotype_ranges <- parallel::mclapply(
+    grch38_vcf,
+    readVcfAsVRanges,
+    genome = "hg38",
+    mc.cores = 4
 )
 
-hg19_alias <- data.table::data.table(
-    refseq = c("NC_000001.10", "NC_000010.10", "NC_000011.9", "NC_000012.11",
-               "NC_000013.10", "NC_000014.8", "NC_000015.9", "NC_000016.9",
-               "NC_000017.10", "NC_000018.9", "NC_000019.9", "NC_000002.11",
-               "NC_000020.10", "NC_000021.8", "NC_000022.10", "NC_000003.11",
-               "NC_000004.11", "NC_000005.9", "NC_000006.11", "NC_000007.13",
-               "NC_000008.10", "NC_000009.11", "NC_000023.10", "NC_000024.9"),
-    ucsc = c("chr1", "chr10", "chr11", "chr12", "chr13", "chr14", "chr15",
-             "chr16", "chr17", "chr18", "chr19", "chr2", "chr20", "chr21",
-             "chr22", "chr3", "chr4", "chr5", "chr6", "chr7", "chr8", "chr9",
-             "chrX", "chrY"),
-    key = "refseq"
+# Keep only the standard chromosome information
+grch37_haplotype_ranges <- parallel::mclapply(
+    grch37_haplotype_ranges,
+    keepStandardChromosomes,
+    mc.cores = 4
 )
-grch38_dt <- hg38_alias[grch38_dt, on = c("refseq"="ReferenceSequence")]
-grch37_dt <- hg19_alias[grch37_dt, on = c("refseq"="ReferenceSequence")]
-
-# Relabel insertions and deletions to match VCF standard
-grch38_dt[Type == "insertion", `Reference Allele` := "<INS>"]
-grch37_dt[Type == "insertion", `Reference Allele` := "<INS>"]
-grch38_dt[Type == "deletion", `Variant Allele` := "<DEL>"]
-grch37_dt[Type == "deletion", `Variant Allele` := "<DEL>"]
-
-# Convert data.tables to GRanges for package
-grch38_gr <- GenomicRanges::makeGRangesFromDataFrame(
-    grch38_dt,
-    keep.extra.columns = TRUE,
-    ignore.strand = TRUE,
-    seqnames.field = "ucsc",
-    start.field = "Variant Start",
-    end.field = "Variant Stop",
-    starts.in.df.are.0based = FALSE,
-    seqinfo = GenomeInfoDb::Seqinfo(genome = "hg38")
+grch38_haplotype_ranges <- parallel::mclapply(
+    grch38_haplotype_ranges,
+    keepStandardChromosomes,
+    mc.cores = 4
 )
-grch37_gr <- GenomicRanges::makeGRangesFromDataFrame(
-    grch37_dt,
-    keep.extra.columns = TRUE,
-    ignore.strand = TRUE,
-    seqnames.field = "ucsc",
-    start.field = "Variant Start",
-    end.field = "Variant Stop",
-    starts.in.df.are.0based = FALSE,
-    seqinfo = GenomeInfoDb::Seqinfo(genome = "hg19")
-)
-grch38_gr <- GenomeInfoDb::keepStandardChromosomes(grch38_gr)
-grch37_gr <- GenomeInfoDb::keepStandardChromosomes(grch37_gr)
 
-# Split by haplotye and gene
-grch38_gene_grl <- split(grch38_gr, f = grch38_gr$Gene)
-grch37_gene_grl <- split(grch37_gr, f = grch37_gr$Gene)
-grch38_haplotype_grl <- split(grch38_gr, f = grch38_gr$`Haplotype Name`)
-grch37_haplotype_grl <- split(grch37_gr, f = grch37_gr$`Haplotype Name`)
+# Coerce to GRangesLists
+grch37_haplotype_grl <- GRangesList(grch37_haplotype_ranges)
+grch38_haplotype_grl <- GRangesList(grch38_haplotype_ranges)
 
-# -----------------------------------------------------------------------------
-# SPECIAL CASE FOR CYP2C19 *2 and *35
-# TODO Figure out a general solution
-cyp2c19_2 <- grch38_haplotype_grl[["CYP2C19*2"]]
-cyp2c19_35 <- grch38_haplotype_grl[["CYP2C19*35"]]
-is_missing <- setdiff(cyp2c19_2, cyp2c19_35)
-cyp2c19_d <- cyp2c19_2[which(cyp2c19_2 == is_missing), ]
-cyp2c19_35 <- c(cyp2c19_35, cyp2c19_d)
-cyp2c19_35$`Variant Allele2` <- c("G", "G", "G")
-grch38_haplotype_grl[["CYP2C19*35"]] <- cyp2c19_35
+# Get per gene ranges ------------------------------------------------------
 
-# Save package data -----------------------------------------------------------
+# Extract all unique gene names from the VCF file names
+grch37_genes <- gsub("data-raw/pharmvar\\-[0-9]+\\.[0-9]+\\.[0-9]+/", "", grch37_vcf)
+grch37_genes <- gsub("\\/GRCh37.*", "", grch37_genes)
+grch37_uniq_genes <- unique(grch37_genes)
+
+grch38_genes <- gsub("data-raw/pharmvar\\-[0-9]+\\.[0-9]+\\.[0-9]+/", "", grch38_vcf)
+grch38_genes <- gsub("\\/GRCh38.*", "", grch38_genes)
+grch38_uniq_genes <- unique(grch38_genes)
+
+# Collapse haplotype ranges into single gene ranges
+grch37_gene_gr <- vector("list", length(grch37_uniq_genes))
+grch38_gene_gr <- vector("list", length(grch38_uniq_genes))
+names(grch37_gene_gr) <- grch37_uniq_genes
+names(grch38_gene_gr) <- grch38_uniq_genes
+
+for (g in grch37_uniq_genes) {
+    keep <- names(grch37_genes[grch37_genes == g])
+    grch37_gene_gr[[g]] <- grch37_haplotype_ranges[keep]
+}
+for (g in grch38_uniq_genes) {
+    keep <- names(grch38_genes[grch38_genes == g])
+    grch38_gene_gr[[g]] <- grch38_haplotype_ranges[keep]
+}
+
+# Create a list of GRangesLists for each gene
+grch37_gene_grl <- lapply(grch37_gene_gr, GRangesList)
+grch38_gene_grl <- lapply(grch38_gene_gr, GRangesList)
+
+# Save as package data ----------------------------------------------------
+
 usethis::use_data(
-  grch38_gene_grl, grch37_gene_grl, grch38_haplotype_grl, grch37_haplotype_grl,
-  overwrite = TRUE, internal = TRUE
+    grch38_gene_grl, grch37_gene_grl, grch38_haplotype_grl, grch37_haplotype_grl,
+    overwrite = TRUE, internal = TRUE
 )
