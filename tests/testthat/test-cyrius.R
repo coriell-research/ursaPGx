23b6605024f22dcb4ba39c32c012ca620741f14b
jcalendo <jcalendo@gmail.com>
2023-05-03 17:22:03 -0400

    Added test for cyrius

---
+++ tests/testthat/test-cyrius.R
@@ -0,0 +1,12 @@
+test_that("Cyrius runs", {
+    # Ensure environment is loaded -- this can be modified later
+    reticulate::use_condaenv("r-reticulate")
+    
+    cram <- list.files(path = here::here("data-raw", "sample_cram"))
+    names(cram) <- gsub(".final", "", tools::file_path_sans_ext(basename(cram)))
+    fa <- here::here("data-raw", "fasta", "GRCh38_full_analysis_set_plus_decoy_hla.fa")
+    result <- cyrius(cram, fa) 
+})
+
+
+
