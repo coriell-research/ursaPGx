2df62309424aa1a01ea0a0d04b464f99105f22d3
jcalendo <jcalendo@protonmail.com>
2023-05-04 21:42:41 -0400

    Check for and create conda env onLoad

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,18 @@
-.onLoad <- function(libname, pkgname) {
-    reticulate::configure_environment(pkgname)
+.onLoad <- function() {
+    message("Checking if miniconda is installed...")
+    if (!file.exists(file.path(reticulate::miniconda_path(), "bin/conda"))) {
+        reticulate::install_miniconda()
+    }
+    
+    message("Checking if 'r-reticulate' environment exists...")
+    envs <- reticulate::conda_list()
+    if (!"r-reticulate" %in% envs) {
+        reticulate::conda_create("r-reticulate")
+        
+        # Install necessary packages
+        reticulate::conda_install(
+            "r-reticulate", 
+            packages = c("pysam", "statsmodels", "scipy"),
+            channel = c("conda-forge", "anaconda", "bioconda"))
+    }
 }
