897e3ce53048cd11c5518f3c1ea84485f06e692b
jcalendo <jcalendo@gmail.com>
2025-03-24 12:43:36 -0400

    Update tests with new genes

--- tests/testthat/test-convertGTtoNucleotides.R
+++ tests/testthat/test-convertGTtoNucleotides.R
@@ -1,18 +1,20 @@
 test_that("convertGTtoNucleotides is able to construct a valid matrix for each sample", {
-  chroms <- c("chr10", "chr19", "chr22", "chr1", "chr11", "chr7", "chr13", "chr12")
+  chroms <- c("chr10", "chr19", "chr22", "chr1", "chr11", "chr7", "chr8", "chr13", "chr15", "chr12")
   d <- here::here("data-raw", "sample_vcf")
   f <- paste0("1kGP_high_coverage_Illumina.", chroms, ".filtered.SNV_INDEL_SV_phased_panel.vcf.gz")
   files <- file.path(d, f)
   names(files) <- chroms
-
+  
   # Mapping for chromosome to genes
   df <- data.frame(
-      chrom = c("chr19", "chr19", "chr19", "chr10", "chr10", "chr10", 
-                "chr7", "chr7", "chr19", "chr13", "chr12"
-      ),
-      gene = c("CYP2A13", "CYP2A6", "CYP2B6", "CYP2C19", "CYP2C8", "CYP2C9", 
-               "CYP3A4", "CYP3A5", "CYP4F2", "NUDT15", "SLCO1B1"
-      )
+    chrom = c("chr19", "chr19", "chr19", "chr10", "chr10", "chr10", 
+              "chr7", "chr7", "chr19", "chr13", "chr12", "chr15", 
+              "chr8"
+    ),
+    gene = c("CYP2A13", "CYP2A6", "CYP2B6", "CYP2C19", "CYP2C8", "CYP2C9", 
+             "CYP3A4", "CYP3A5", "CYP4F2", "NUDT15", "SLCO1B1", "CYP1A2", 
+             "NAT2"
+    )
   )
 
   # Read in all gene data and test if each gene has callable alleles
